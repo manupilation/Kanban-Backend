8056e5aeb0b4edaf1c21d9dfd9aabda6ec2f4a60
Emanoel Mattos <emanoelmattos13@gmail.com>
2022-12-10 01:40:45 -0300

    refactor: bcrypt methods

--- src/utils/password.js
+++ src/utils/password.js
@@ -1,14 +1,14 @@
-import { hash, compare} from "bcryptjs";
+import bcrypt from "bcryptjs";
 
 export default class BCryptParse {
   static async hashPassword(pass) {
-    const encryptedPass = await hash(pass, 10);
+    const encryptedPass = await bcrypt.hash(pass, 10);
 
     return encryptedPass;
   }
 
   static async comparePassword(pass, hashedPass) {
-    const comparingPassword = await compare(pass, hashedPass);
+    const comparingPassword = await bcrypt.compare(pass, hashedPass);
 
     return comparingPassword;
   }
