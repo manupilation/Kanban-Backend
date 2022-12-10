9f3ee9c560bf4f7c802ba3934b920afd51aed9f6
Emanoel Mattos <emanoelmattos13@gmail.com>
2022-12-10 00:28:34 -0300

    feat: using dataRouter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import express from "express";
 import connector from "./database/connection.js";
+import dataRouter from "./routes/dataRouter.js";
+import bodyParser from "body-parser";
 
 class App {
   constructor() {
     this.app = express();
     this.db = connector().catch(err => console.error(err));
+    this.app.use(bodyParser.json());
+    this.routes();
   }
 
   start(port) {
     this.app.listen(port, () => {
       console.log(`Task scheduler is running on port ${port}`);
     });
+  }
+
+  routes() {
+    this.app.use("", dataRouter);
   }
 }
 
