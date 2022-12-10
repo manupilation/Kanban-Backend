49aca7e6ef1ada289347bd7bd2ab89ee05b75d03
Emanoel Mattos <emanoelmattos13@gmail.com>
2022-12-10 01:26:04 -0300

    feat: getData controller

--- src/controllers/dataController.js
+++ src/controllers/dataController.js
@@ -2,12 +2,20 @@ import DataService from "../services/dataService.js";
 
 class DataController {
   async setUser(req, res) {
-    const { user, email, password, tasks } = req.body;
+    const { user, email, password, tasks = [] } = req.body;
     const setData = new DataService();
 
     const final = await setData.setUser({ user, email, password, tasks });
 
     res.status(201).json(final);
+  }
+
+
+  async getData(req, res) {
+    const { id } = req.body;
+    const dbdata = await new DataService().getData(id);
+
+    res.status(200).json(dbdata);
   }
 }
 
