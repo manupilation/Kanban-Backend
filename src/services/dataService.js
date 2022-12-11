87fb4c5527d10e82ba2f5712c47062021469113c
Emanoel Mattos <emanoelmattos13@gmail.com>
2022-12-11 00:11:22 -0300

    feat: login service

--- src/services/dataService.js
+++ src/services/dataService.js
@@ -1,20 +1,51 @@
 import DataModel from "../models/dataModel.js";
 import BCryptParse from "../utils/password.js";
+import validationSchema from "../controllers/joi/validateSchema.js";
+import userSchema from "../controllers/joi/dataUser.js";
+import { loginErrorPassword } from "../utils/loginError.js";
+import JWTMethods from "../utils/jwt.js";
 
 class DataService {
   async setUser({user, email, password, tasks}) {
     const setData = new DataModel();
     const hashPassword = await BCryptParse.hashPassword(password);
-    const data = await setData.setUser({user, email, password: hashPassword, tasks});
+
+    try {
+      validationSchema(userSchema, {user, email, password});
+      const data = await setData.setUser({user, email, password: hashPassword, tasks});
+
+      return data;
+    } catch(err) {
+      console.error(err);
+      throw err;
+    }
+  }
+
+  async getData(id) {
+    const dbdata = new DataModel();
+    const data = await dbdata.getData(id);
 
     return data;
   }
 
-  async getData(id) {
-    const dbdata = new DataModel;
-    const data = await dbdata.getData(id);
+  async login({ password, email }) {
+    try {
+      const acessDb = new DataModel();
+      const { password: dbPass, email: dbEmail, user, id } = await acessDb.login(email);
+      const compare = await BCryptParse.comparePassword(password, dbPass);
 
-    return data;
+      if(compare == false) {
+        loginErrorPassword();
+      }
+
+      const token = JWTMethods.jwtSignature({id, user, email: dbEmail});
+
+      return token;
+
+    } catch(err) {
+      console.log(err);
+      throw err;
+    }
   }
 }
 
