f7d44e1dbb8a212e783771d682920af56c13b528
Emanoel Mattos <emanoelmattos13@gmail.com>
2022-12-11 00:11:32 -0300

    feat: login controller

--- src/controllers/dataController.js
+++ src/controllers/dataController.js
@@ -1,13 +1,17 @@
 import DataService from "../services/dataService.js";
 
 class DataController {
-  async setUser(req, res) {
-    const { user, email, password, tasks = [] } = req.body;
-    const setData = new DataService();
-
-    const final = await setData.setUser({ user, email, password, tasks });
-
-    res.status(201).json(final);
+  async setUser(req, res, next) {
+    try {
+      const { user, email, password, tasks = [] } = req.body;
+      const setData = new DataService();
+  
+      const final = await setData.setUser({ user, email, password, tasks });
+  
+      res.status(201).json(final);
+    } catch(err) {
+      next(err);
+    }
   }
 
 
@@ -16,6 +20,16 @@ class DataController {
     const dbdata = await new DataService().getData(id);
 
     res.status(200).json(dbdata);
+  }
+
+  async login(req, res, next) {
+    try {
+      const connect = await new DataService().login(req.body);
+
+      res.status(200).json({ token: connect });
+    } catch(err) {
+      next(err);
+    }
   }
 }
 
