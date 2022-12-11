6ab229d78d4d0f32857eba9da4548ea45e6e87c1
Emanoel Mattos <emanoelmattos13@gmail.com>
2022-12-11 00:11:39 -0300

    feat: login route

--- src/routes/dataRouter.js
+++ src/routes/dataRouter.js
@@ -5,6 +5,7 @@ const dataRouter = express.Router();
 
 dataRouter.post("/", new DataController().setUser);
 dataRouter.post("/get", new DataController().getData);
+dataRouter.post("/login", new DataController().login);
 // Esta rota será get, o id será pego pelo token;
 // Parece que um id inválido retorna uma promise rejection
 
