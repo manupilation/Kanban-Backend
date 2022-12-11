33998b2bcdafa72b8fbd94c931b0f90b8e99ccbd
Emanoel Mattos <emanoelmattos13@gmail.com>
2022-12-11 00:08:27 -0300

    feat: error middleware

---
+++ src/controllers/middlewares/erros.js
@@ -0,0 +1,28 @@
+import Joi from "joi";
+
+function defineErrorStatus(name) {
+  switch (name) {
+  case "badRequest":
+    return 400;
+  case "invalidInput":
+    return 422;
+  case "Unauthorized":
+    return 401;
+  default:
+    return 500;
+  }
+}
+
+function handlerRouteError(err, _req, res, next) {
+
+  if(Joi.isError(err)) {
+    next(err);
+  }
+
+  const status = defineErrorStatus(err.name);
+  console.log(status);
+
+  res.status(status).json({ error: err.message });
+}
+
+export default handlerRouteError;
